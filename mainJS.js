df45d67e722a7c3e217bb61ba1648549dff74faa
Fabian Torres <46858378+fabiantorrestech@users.noreply.github.com>
2023-08-25 12:54:55 -0500

    late commit with lots of game logic in mainJS.js....

---
+++ mainJS.js
@@ -0,0 +1,70 @@
+// randomly selects a choice of rock, paper, or scissors
+function getComputerChoice(){
+  let choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+
+// - holds all the logic for the game, once the player has selected an option and a random choice has been made by the computer
+function playRound(playerSelection, computerSelection){
+
+  console.log("playerSelection: " + playerSelection + " | computerSelection: " + computerSelection);
+
+  // - case insenstive for user-input (playerSelection)
+  playerSelection = playerSelection.toLowerCase();
+
+  let verdict = "";
+
+  if(playerSelection==="rock" && computerSelection==="scissors" ||
+  playerSelection==="paper" && computerSelection==="rock" ||
+  playerSelection==="scissors" && computerSelection==="paper"){
+    verdict = "You win!";
+  }
+  else if(playerSelection==="rock" && computerSelection==="rock" ||
+  playerSelection==="paper" && computerSelection==="paper" ||
+  playerSelection==="scissors" && computerSelection==="scissors"){
+    verdict = "It's a tie.";
+  }
+  else{
+    verdict = "You lose!";
+  }
+
+  return verdict;
+
+}
+
+function updateScore(playerScore, computerScore, verdict){
+  if(verdict==="You lose!"){
+    computerScore++;
+  }
+  else if(verdict==="You win!"){
+    playerScore++;
+  }
+}
+
+function game(){
+  let playerScore = 0;
+  let computerScore = 0;
+  let playerSelection = "";
+
+  console.log("Welcome to Rock, Paper, Scissors!");
+  for(let i=0; i<5; i++){
+    // - best 3/5, so we play minimum 
+    if((playerScore===3 && computerScore<=2) ||
+    (computerScore===3 && playerScore<=2)){
+      break;
+    }
+    
+    // TODO: keep prompting the player until they enter a valid option
+    playerSelection = prompt("Type out a choice: rock, paper, scissors.");
+    console.log("Player has chosen: " + playerSelection);
+    
+    console.log(playRound(playerSelection, getComputerChoice()));
+
+    console.log("Score -- Player: " + playerScore + " | Computer: " + computerScore);
+  }
+
+  if(playerScore > computerScore){ return "Player wins! (:"; }
+  else if(playerScore < computerScore){ return "Computer wins! ):" }
+  else{ return "It's a tie!" }
+
+}
